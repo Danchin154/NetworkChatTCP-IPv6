6107eb7c3d357987b1c81f1935e785b91a5fcdec
Danchin154 <73567310+Danchin154@users.noreply.github.com>
2023-05-04 19:03:36 +0800

    Add files via upload

---
+++ ChatTCI/ChatTCI/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+
+    class Program
+    {
+        static Dictionary<TcpClient, string> clients = new Dictionary<TcpClient, string>();
+
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.IPv6Any, 8888);
+            listener.Start();
+            Console.WriteLine("Сервер запущен и ожидает подключение.");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                byte[] nameBuffer = new byte[1024];
+                int bytesRead;
+                bytesRead = client.GetStream().Read(nameBuffer, 0, nameBuffer.Length);
+                string name = Encoding.UTF8.GetString(nameBuffer, 0, bytesRead);
+
+                clients.Add(client, name);
+
+                Console.WriteLine("{0} подключился к чату", name);
+
+                Thread thread = new Thread(() => HandleClient(client));
+                thread.Start();
+            }
+        }
+
+        static void HandleClient(TcpClient client)
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = client.GetStream().Read(buffer, 0, buffer.Length);
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                    if (string.IsNullOrEmpty(message))
+                    {
+                        clients.Remove(client);
+                        Console.WriteLine("{0} покинул чат", clients[client]);
+                        break;
+                    }
+
+                    Console.WriteLine("{0}: {1}", clients[client], message);
+
+                    foreach (KeyValuePair<TcpClient, string> pair in clients)
+                    {
+                        if (pair.Key != client)
+                        {
+                            string output = string.Format("{0}: {1}", clients[client], message);
+                            byte[] outputBuffer = Encoding.UTF8.GetBytes(output);
+                            pair.Key.GetStream().Write(outputBuffer, 0, outputBuffer.Length);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+}
+
---
+++ ChatTCI/Client/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            TcpClient client = new TcpClient(AddressFamily.InterNetworkV6);
+            client.Connect(IPAddress.IPv6Loopback, 8888);
+
+            byte[] nameBuffer = Encoding.UTF8.GetBytes(name);
+            client.GetStream().Write(nameBuffer, 0, nameBuffer.Length);
+
+            Thread receiveThread = new Thread(() => ReceiveMessages(client));
+            receiveThread.Start();
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                client.GetStream().Write(buffer, 0, buffer.Length);
+            }
+        }
+
+        static void ReceiveMessages(TcpClient client)
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = client.GetStream().Read(buffer, 0, buffer.Length);
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
